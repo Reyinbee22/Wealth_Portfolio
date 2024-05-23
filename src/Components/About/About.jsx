5e754fe75ae761f57c26ed49667825a6b7ee5257
Wealth <basseyiyene@gmail.com>
2024-05-23 11:16:39 +0100

    main

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import image2_icon from '../../assets/image2_icon.jpg'
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern}alt="" />
+        </div>
+        
+        <div className="about-sections">
+        <div className="about-left">
+        <img src={image2_icon} alt="" />
+
+        </div>
+        <div className="about-right">
+            <div className="about-para">
+                <p>As a full stack developer, I thrive on architecting comprehensive solutions that seamlessly integrate front-end and back-end technologies to deliver robust and user-centric web applications.</p>
+                <p>Coding across the entire software stack, I orchestrate innovative solutions that harmonize user experience with backend functionality, ensuring versatile and efficient web applications.</p>
+            </div>
+            <div className="about-skills">
+                <div className="about-skill"><p>HTML&CSS</p><hr style={{width:'50%'}}/></div>
+                <div className="about-skill"><p>React JS</p><hr style={{width:'70%'}}/></div>
+                <div className="about-skill"><p>JavaScript</p><hr style={{width:'60%'}}/></div>
+                <div className="about-skill"><p>Next JS</p><hr style={{width:'50%'}}/></div>
+
+            </div>
+
+        </div>
+        </div>
+        
+        
+      <div className="about-achievements">
+        <div className="about-achievement">
+      <h1>2MONTHS+</h1>
+     <p>MONTHS OF EXPERIENCE</p>
+        </div>
+        <hr/>
+        <div className="about-achievement">
+     <h1>2+</h1>
+     <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+<h1>2+</h1>
+<p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer-top">
+        <div className="footer-top-left">
+            {/* <img src={ footer_logo} alt="" /> */}
+            <p>As a burgeoning full stack developer with two months of immersive experience, hailing from Nigeria and having honed my skills at Univelcity, I'm poised to innovate and create impactful solutions in the tech landscape.</p>
+        </div>
+        <div className="footer-top-right">
+            <div className="footer-email-input">
+                <img src={user_icon} alt="" />
+                <input type="email" placeholder='Enter Your Email' />
+            </div>
+           <div className="footer-subcribe">Subcribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className='footer-bottom-left'>© 2024 Wealth Bassey. All rights reserved.</p>
+        <div className="footer-bottom-right">
+            <p>Terms of Services</p>
+            <p>Privacy Policy</p>
+            <p>Connect With Me</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
