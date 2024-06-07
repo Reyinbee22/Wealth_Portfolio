092cf61ec4536b459980eaeedb68077ec74ce7bd
Wealth <basseyiyene@gmail.com>
2024-06-07 16:31:02 +0100

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Services from './Components/Services/Services'
-import MyWork from './Components/MyWork/MyWork'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import Navbar from './Components/Navbar/Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
+import Services from './Components/Services/Services';
+import MyWork from './Components/MyWork/MyWork';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   const handleConnectClick = () => {
-    // Implement the action you want to perform when "Connect With Me" is clicked
-    console.log("Connect With Me clicked!"); // For debugging
+    console.log("Connect With Me!");
   };
-  
+
   return (
-    <div>
-      <Navbar  handleConnectClick={handleConnectClick} />
-      <Hero  handleConnectClick={handleConnectClick} /> 
-      <About/>
-      <Services/>
-      <MyWork/>
-      <Contact/>
-      <Footer/>
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar handleConnectClick={handleConnectClick} />
+        <Routes>
+          <Route path="/" element={<Hero handleConnectClick={handleConnectClick} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/work" element={<MyWork />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../../assets/logo.svg';
 import underline from '../../assets/nav_underline.svg';
 
-const Navbar = () => {
+const Navbar = ({ handleConnectClick }) => {
   const [menu, setMenu] = useState("home");
 
   const handleMenuItemClick = (section) => {
@@ -12,41 +13,47 @@ const Navbar = () => {
     document.getElementById(section).scrollIntoView({ behavior: 'smooth' });
   };
 
-  const handleConnectClick = () => {
-    // Implement the action you want to perform when "Connect With Me" is clicked
-    console.log("Connect With Me clicked!");
-    // For example, you can open a contact form or navigate to a contact page
-  };
-
   return (
     <div className='navbar'>
       {/* <img src={logo} alt="" /> */}
       <ul className="nav-menu">
         <li>
-          <p onClick={() => handleMenuItemClick("home")}>Home</p>
-          {menu === "home" ? <img src={underline} alt=''/> : null}
+          <Link to="/" onClick={() => handleMenuItemClick("home")}>
+            Home
+          </Link>
+          {menu === "home" ? <img src={underline} alt='' /> : null}
         </li>
         <li>
-          <p onClick={() => handleMenuItemClick("about")}>About Me</p>
-          {menu === "about" ? <img src={underline} alt=''/> : null}
+          <Link to="/about" onClick={() => handleMenuItemClick("about")}>
+            About Me
+          </Link>
+          {menu === "about" ? <img src={underline} alt='' /> : null}
         </li>
         <li>
-          <p onClick={() => handleMenuItemClick("services")}>Services</p>
-          {menu === "services" ? <img src={underline} alt=''/> : null}
+          <Link to="/services" onClick={() => handleMenuItemClick("services")}>
+            Services
+          </Link>
+          {menu === "services" ? <img src={underline} alt='' /> : null}
         </li>
         <li>
-          <p onClick={() => handleMenuItemClick("work")}>Portfolio</p>
-          {menu === "work" ? <img src={underline} alt=''/> : null}
+          <Link to="/work" onClick={() => handleMenuItemClick("work")}>
+            Portfolio
+          </Link>
+          {menu === "work" ? <img src={underline} alt='' /> : null}
         </li>
         <li>
-          <p onClick={() => handleMenuItemClick("contact")}>Contact</p>
-          {menu === "contact" ? <img src={underline} alt=''/> : null}
+          <Link to="/contact" onClick={() => handleMenuItemClick("contact")}>
+            Contact
+          </Link>
+          {menu === "contact" ? <img src={underline} alt='' /> : null}
         </li>
       </ul>
-      <div className="nav-connect" onClick={handleConnectClick}>Connect With Me</div>
+      <div className="nav-connect" onClick={handleConnectClick}>
+        Connect With Me
+      </div>
     </div>
   );
-}
+};
 
 export default Navbar;
 
